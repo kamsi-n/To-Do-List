e7260467c4a419d5f00c9f173f21ad26c9d51db7
kamsi-n01~ <nwokorokamsi21@gmail.com>
2023-04-25 12:48:01 +0100

    add 2 more inputs, style them and add javascript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let checked = document.getElementById('check');
-
+let checked2 = document.getElementById('check2');
+let checked3 = document.getElementById('check3');
 
 function crossOut() {
     let userInput = document.getElementById('userInput');
@@ -7,4 +8,19 @@ function crossOut() {
     userInput.style.border = 'none';   
 }
 
+function crossOut2() { 
+    let userInput2 = document.getElementById('userInput2');
+    userInput2.style.textDecoration = 'line-through';
+    userInput2.style.border = 'none';
+}
+
+function crossOut3() {
+    let userInput3 = document.getElementById('userInput3');
+    userInput3.style.textDecoration = 'line-through';
+    userInput3.style.border = 'none';  
+}
+
 checked.addEventListener('click', crossOut)
+checked2.addEventListener('click', crossOut2)
+checked3.addEventListener('click', crossOut3)
+
