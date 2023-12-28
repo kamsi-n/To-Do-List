f971b745c0e4291d69cc4ba032ca8c78621ad0dd
Kamsi <nwokorokamsi21@gmail.com>
2023-12-28 12:46:59 +0000

    restore deleted files

--- script.js
+++ script.js
@@ -1,71 +1,48 @@
-//the user iput cross-through
-let checked = document.getElementById('check');
-let checked2 = document.getElementById('check2');
-let checked3 = document.getElementById('check3');
+const inputTask = document.getElementById('inputTask');
+const inputBtn = document.getElementById('inputBtn');
+let error = document.getElementById('msg');
+let posts = document.getElementById('tasks');
+const emptyTask = document.getElementById('complete');
 
-function crossOut() {
-    let userInput = document.getElementById('userInput');
-    userInput.style.textDecoration = 'line-through';
-    userInput.style.border = 'none';   
-}
+inputBtn.addEventListener('click', ()=> {
+    if (inputTask.value === '') {
+        error.innerHTML = 'Please enter a task';
+    } else {
+        error.innerHTML = '';
+        acceptData();
+        taskRef.push(tasks);
+    }
+});
 
-function crossOut2() { 
-    let userInput2 = document.getElementById('userInput2');
-    userInput2.style.textDecoration = 'line-through';
-    userInput2.style.border = 'none';
-}
+let data = {};
+let acceptData = () => {
+    data['text'] = inputTask.value;
+    createPost();
+};
 
-function crossOut3() {
-    let userInput3 = document.getElementById('userInput3');
-    userInput3.style.textDecoration = 'line-through';
-    userInput3.style.border = 'none';  
-}
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span>
+            <i onClick="editPost(this)" class="fa-regular fa-pen-to-square"></i>
+            <i onclick="complete(this)" class="fa-regular fa-circle-check"></i>
+            <i onClick="deletePost(this)" class="fa-solid fa-trash-can"></i>
+        </span>
+    </div>
+    `;
+    inputTask.value = '';
+};
 
-checked.addEventListener('click', crossOut);
-checked2.addEventListener('click', crossOut2);
-checked3.addEventListener('click', crossOut3);
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
 
-//Program the 1st clear button
-let btn1 = document.getElementById('btn1');
+let editPost = (e) => {
+    inputTask.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+};
 
-function clear() {
-    userInput.value = '';
-}
-
-function restoreBorder() {
-    userInput.style.border = '1px solid';
-    checked.checked = false;
-}
-
-btn1.addEventListener('click' , clear);
-btn1.addEventListener('click' , restoreBorder);
-
-//Program the 2nd clear button
-let btn2 = document.getElementById('btn2');
-
-function clear2() {
-    userInput2.value = '';
-}
-
-function restoreBorder2() {
-    userInput2.style.border = '1px solid';
-    checked2.checked = false;
-}
-
-btn2.addEventListener('click' , clear2);
-btn2.addEventListener('click' , restoreBorder2);
-
-//Program the 3rd clear button
-let btn3 = document.getElementById('btn3');
-
-function clear3() {
-    userInput3.value = '';
-}
-
-function restoreBorder3() {
-    userInput3.style.border = '1px solid';
-    checked3.checked = false;
-}
-
-btn3.addEventListener('click' , clear3);
-btn3.addEventListener('click' , restoreBorder3);
+let complete = (e) => {
+    e.parentElement.previousElementSibling.style.textDecoration = 'line-through'
+};
